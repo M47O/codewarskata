7645bf19b0108388ce9766ff844acb8b671fcf8d
Mason Turner <turner_m@lynchburg.edu>
2022-09-22 17:00:34 -0400

    Add theHashtagGenerator.js

---
+++ 5kyu/theHashtagGenerator.js
@@ -0,0 +1,35 @@
+/* The marketing team is spending way too much time typing in hashtags.
+Let's help them with our own Hashtag Generator!
+
+Here's the deal:
+
+It must start with a hashtag (#).
+All words must have their first letter capitalized.
+If the final result is longer than 140 chars it must return false.
+If the input or the result is an empty string it must return false.
+
+Examples
+	" Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+	"    Hello     World   "                  =>  "#HelloWorld"
+	""                                        =>  false
+*/
+
+//Solution:
+function generateHashtag (str) {
+  const words = str.trim().split(" ")
+  const hashtag = ["#"]
+
+  if(str.trim() == ""){
+    return false
+  } else {
+    hashtag.push(...words.map(x => {
+      return x.charAt(0).toUpperCase() + x.slice(1)
+      })
+    )
+  }
+  if(hashtag.join("").length > 140){
+    return false
+  } else {
+    return hashtag.join("")
+  }
+}
