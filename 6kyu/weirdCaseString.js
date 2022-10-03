1e58be0983a1eba04d86f6b89c35e7c128285fdf
Mason Turner <turner_m@lynchburg.edu>
2022-10-03 12:09:47 -0400

    Add weirdCaseString.js

---
+++ 6kyu/weirdCaseString.js
@@ -0,0 +1,27 @@
+/* Write a function toWeirdCase (weirdcase in Ruby) that accepts a string, and returns the same string with all even indexed characters in each word upper cased, and all odd indexed characters in each word lower cased. The indexing just explained is zero based, so the zero-ith index is even, therefore that character should be upper cased and you need to start over for each word.
+
+The passed in string will only consist of alphabetical characters and spaces(' '). Spaces will only be present if there are multiple words. Words will be separated by a single space(' ').
+
+Examples:
+	toWeirdCase( "String" );//=> returns "StRiNg"
+	toWeirdCase( "Weird string case" );//=> returns "WeIrD StRiNg CaSe" */
+
+
+//Solution:
+function toWeirdCase(string){
+  //array containing arrays of letters
+  const splitWords = string.split(" ").map(x=> x.split(""))
+  //map over the array of words, with the nested map iterating over each letter
+  return splitWords.map(word => {                                 
+    return word.map((letter, index) => {
+      if(index % 2 == 0){
+        return letter = letter.toUpperCase()
+      } else {
+        return letter = letter.toLowerCase()
+      }
+      //Second level map closes here and each element (letter) is joined into one string (word)
+    }).join("") 
+      //First level map closes here and each element (the separate words) is joined into one string, separated by a space
+  }).join(" ")
+  
+}
