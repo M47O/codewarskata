fe9b24901074a969c106de7ecc938b8eba8b63ff
Mason Turner <turner_m@lynchburg.edu>
2022-11-06 12:25:44 -0500

    Add sortArrays-1.js

---
+++ 7kyu/sortArrays-1.js
@@ -0,0 +1,10 @@
+/* Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order. */
+
+//Solution:
+	// input: names - unsorted array
+	// output: sorted array
+sortme = function( names ){
+  return names.sort((a,b) => {
+    if(a < b) return -1
+  })
+}
