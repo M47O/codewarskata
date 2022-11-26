095c60c8de57cf5e9e9b101c3785e9c8c31a6002
Mason Turner <turner_m@lynchburg.edu>
2022-11-26 08:02:13 -0500

    Add nthPowerRulesThemAll.js

---
+++ 7kyu/nthPowerRulesThemAll.js
@@ -0,0 +1,15 @@
+/* You are provided with an array of positive integers and an additional integer n (n > 1).
+
+Calculate the sum of each value in the array to the nth power. Then subtract the sum of the original array.
+
+Examples
+	{1, 2, 3}, 3  -->  (1^3 + 2^3 + 3^3 ) - (1 + 2 + 3)  -->  36 - 6  -->  30
+	{1, 2}, 5     -->  (1^5 + 2^5) - (1 + 2)             -->  33 - 3  -->  30 */
+
+//Solution:
+function modifiedSum(a, n) {
+ const nth = a.map(x => x**n)
+ const nthSum = nth.reduce((a,cv) => a + cv, 0)
+ const aSum = a.reduce((a,cv) => a + cv, 0)
+ return nthSum-aSum
+ }
