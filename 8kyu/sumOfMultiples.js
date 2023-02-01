a4ea96061af0951b60b3a34c474ad7db7f7fba50
Mason Turner <turner_m@lynchburg.edu>
2023-02-01 13:12:11 -0500

    Add sumOfMultiples.js

---
+++ 8kyu/sumOfMultiples.js
@@ -0,0 +1,26 @@
+/* Find the sum of all multiples of n below m
+
+Keep in Mind
+n and m are natural numbers (positive integers)
+m is excluded from the multiples
+
+Examples
+	sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+	sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+	sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+	sumMul(4, -7)  ==> "INVALID" */
+
+//Soltuino:
+function sumMul(n,m){
+  let multiples = []
+  
+  if(0 >= n || 0 >= m){
+    return "INVALID"
+  }
+  
+  for(let i = 1; n*i < m; i++){
+    multiples.push(n * i)
+  }
+  
+  return multiples.reduce((acc, cv) => acc + cv, 0)
+}
