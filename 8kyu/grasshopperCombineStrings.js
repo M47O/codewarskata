4083f7affe572be63fb1b3bc188ccc352fdb944c
Mason Turner <turner_m@lynchburg.edu>
2023-03-01 09:38:38 -0500

    Add grasshopperCombineStrings.js

---
+++ 8kyu/grasshopperCombineStrings.js
@@ -0,0 +1,10 @@
+/* Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+	combineNames('James', 'Stevens')
+
+returns:
+	'James Stevens' */
+
+//Solution:
+const combineNames = (firstName, lastName) => firstName + ' ' + lastName
