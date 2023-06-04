3a7aff9282dd47529f40e2787e8f11abc262f414
Mason Turner <turner_m@lynchburg.edu>
2023-06-04 09:39:24 -0400

    Add unfinishedLoopBugFixing1.js

---
+++ 8kyu/unfinishedLoopBugFixing1.js
@@ -0,0 +1,23 @@
+/* Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop! */
+
+//Original Code:
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number;){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
+
+//Solution:
+function createArray(number){
+  var newArray = [];
+  
+  for(var counter = 1; counter <= number; counter++){
+    newArray.push(counter);
+  }
+  
+  return newArray;
+}
