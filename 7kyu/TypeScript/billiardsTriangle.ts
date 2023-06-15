ecd2a0900c04f5ca4110782441fe88ebe44112ec
Mason Turner <turner_m@lynchburg.edu>
2023-06-15 10:14:04 -0400

    Add billiardsTriangles.ts

--- 7kyu/TypeScript/billiardsTriangle.ts
+++ 7kyu/TypeScript/billiardsTriangle.ts
@@ -1,6 +1,6 @@
 /* Remember the triangle of balls in billiards? To build a classic triangle (5 levels) you need 15 balls. With 3 balls you can build a 2-level triangle, etc.
 
-For more examples,
+For more examples:
 	pyramid(1) == 1
 	pyramid(3) == 2
 	pyramid(6) == 3
