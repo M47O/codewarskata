dec17e7c2fed21fc9e70e6288c7e940584fed30b
Mason Turner <turner_m@lynchburg.edu>
2023-07-15 09:44:13 -0400

    Add evensAndOdds.ts

---
+++ 7kyu/TypeScript/evensAndOdds.ts
@@ -0,0 +1,11 @@
+/* This kata is about converting numbers to their binary or hexadecimal representation:
+
+If a number is even, convert it to binary.
+If a number is odd, convert it to hex.
+Numbers will be positive. The hexadecimal string should be lowercased.
+*/
+
+//Solution:
+export function evensAndOdds(n: number): string {
+  return n % 2 === 0 ? n.toString(2) : n.toString(16)
+}
