87d5ecb4e3f46c89a400b2ccbf287ee0e45d52a4
Mason Turner <turner_m@lynchburg.edu>
2023-07-31 12:54:41 -0400

    Add substitutingVariablesIntoStringsPaddedNumbers.js

---
+++ 7kyu/JavaScript/substitutingVariablesIntoStringsPaddedNumbers.js
@@ -0,0 +1,15 @@
+/* Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+	Example:
+	solution(5) // should return "Value is 00005" */
+
+//Solution:
+function solution(value){
+  const difference = 5 - String(value).length
+  const valueArr = String(value).split("")
+  
+  for(let i = 0; i < difference; i++){
+    valueArr.unshift("0")
+  }
+  
+  return `Value is ${valueArr.join("")}`
+}
