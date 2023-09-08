6c15804cf0033876c0783b021b6dd676d35ca4a1
Mason Turner <turner_m@lynchburg.edu>
2023-09-08 08:31:15 -0400

    Add areaOfACircle.ts

---
+++ 7kyu/TypeScript/areaOfACircle.ts
@@ -0,0 +1,16 @@
+/* Complete the function which will return the area of a circle with the given radius.
+Returned value is expected to be accurate up to tolerance of 0.01.
+If the radius is not positive, throw Error.
+
+Example:
+	circleArea(43.2673);     // returns 5881.248  (± 0.01)
+	circleArea(68);          // returns 14526.724 (± 0.01)
+	circleArea(0);           // throws Error
+	circleArea(-1);          // throws Error */
+
+//Solution:
+export function circleArea(radius: number): number {
+  if(radius <= 0) throw new Error
+  
+  return +(Math.PI * radius ** 2).toFixed(3)
+}
