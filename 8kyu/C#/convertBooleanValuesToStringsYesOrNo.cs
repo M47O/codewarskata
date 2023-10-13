c9f6b64a6dcf9e06674c8c5296f46e4ac7c718c4
Mason Turner <turner_m@lynchburg.edu>
2023-10-13 13:24:50 -0400

    Add convertBooleanValuesToStringsYesOrNo.cs

---
+++ 8kyu/C#/convertBooleanValuesToStringsYesOrNo.cs
@@ -0,0 +1,12 @@
+/* Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. */
+
+//Solution:
+using System;
+
+public static class Kata
+{
+  public static string boolToWord(bool word)
+  {
+    return word ? "Yes" : "No";
+  }
+}
