3a8c459c16badc1a96bfb8c089c35a9f8a737085
Mason Turner <turner_m@lynchburg.edu>
2023-10-14 13:12:38 -0400

    Add vowelCount.cs

---
+++ 7kyu/C#/vowelCount.cs
@@ -0,0 +1,24 @@
+/* Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces. */
+
+//Solution:
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u'};
+        int vowelCount = 0;
+      
+        foreach(char c in str){
+          if(vowels.Contains(c)){
+            vowelCount++;
+          }
+        }
+
+        return vowelCount;
+    }
+}
