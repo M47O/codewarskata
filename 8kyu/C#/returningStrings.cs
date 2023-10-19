1199941803f2c66d1114f1fc4b0fc93c54e16d04
Mason Turner <turner_m@lynchburg.edu>
2023-10-19 10:24:19 -0400

    Add returningStrings.cs

---
+++ 8kyu/C#/returningStrings.cs
@@ -0,0 +1,7 @@
+/* Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?". */
+
+//Solution: 
+public static class Kata
+{
+  public static string Greet(string name) => $"Hello, {name} how are you doing today?";
+}
