bc25d6d4a3ce8577d9127373b3a94c98e7f8469c
Mason Turner <turner_m@lynchburg.edu>
2023-10-23 16:54:25 -0400

    Add convertABooleanToAString.ts

---
+++ 8kyu/TypeScript/convertABooleanToAString.ts
@@ -0,0 +1,7 @@
+/* Implement a function which convert the given boolean value into its string representation.
+Note: Only valid inputs will be given. */
+
+//Solution:
+export const booleanToString = (b:boolean):string => {
+  return b.toString()
+};
